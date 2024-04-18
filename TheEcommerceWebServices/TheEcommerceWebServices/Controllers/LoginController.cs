3f61755aa79abca6663a9e267a1ae0c1219f0554
Henrique Dário <henrique.dariov@hotmail.com>
2024-04-18 01:36:59 -0300

    Base for a login microservice

---
+++ TheEcommerceWebServices/TheEcommerceWebServices/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheEcommerceWebServices.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
